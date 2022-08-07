980176b483350ff1df86e802b09ab7f36f43a5b9
Simon Vargas <sjvargas01@gmail.com>
2022-08-06 21:40:33 -0400

    updated home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,6 +18,39 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: (){},
         child: const Icon(Icons.add),
       ),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 80.0),
+          itemCount: 1,
+          itemBuilder: (BuildContext context, int index){
+          if(index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text(
+                        'My Notes',
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10.0,),
+                      Text(
+                        '0 of 10',
+                        style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                ),
+            );
+            }
+          }
+      ),
     );
   }
 }
